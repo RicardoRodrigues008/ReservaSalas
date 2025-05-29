8e804ce49915ec1be94594613b88a33e2a5f82f7
gabriel <gabriel.silva1089@aluno.ce.gov.br>
2025-05-29 11:12:39 -0300

    Conexão com banco de dados feita

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <?php
+        require_once('./config/database/database.php');
+    ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de Reserva de Salas</title>
